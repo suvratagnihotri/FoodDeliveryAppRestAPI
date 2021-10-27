63994675465dab529a060cc971df8f5d3c45a5d9
Suvrat <suvrat72000@gmail.com>
2021-10-27 23:32:06 +0530

    Update commit

--- src/main/java/com/food/delivery/CustomerRepository.java
+++ src/main/java/com/food/delivery/CustomerRepository.java
@@ -1,5 +1,6 @@
 package com.food.delivery;
 
+//Customer repository to retrieve and add cusotmers in to the database.
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer>{

0a0596dcb42d808a0adee674bdfc3746b58e8fee
Suvrat <suvrat72000@gmail.com>
2021-10-30 22:33:30 +0530

    Update commit

--- src/main/java/com/food/delivery/FooddeliveryApplication.java
+++ src/main/java/com/food/delivery/FooddeliveryApplication.java
@@ -3,6 +3,8 @@ package com.food.delivery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//Main class , from where the execution of the application starts.
 @SpringBootApplication
 public class FooddeliveryApplication {
 

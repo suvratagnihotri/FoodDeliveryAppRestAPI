3d305216fe57c5da7339d9ba2d458da6dd744668
Suvrat <suvrat72000@gmail.com>
2021-10-30 22:34:10 +0530

    Update commit

--- src/main/java/com/food/delivery/FoodRepository.java
+++ src/main/java/com/food/delivery/FoodRepository.java
@@ -1,5 +1,6 @@
 package com.food.delivery;
 
+//Food repository class, to perform CRUD operations in to the databse.
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FoodRepository extends JpaRepository<Food,Integer>{

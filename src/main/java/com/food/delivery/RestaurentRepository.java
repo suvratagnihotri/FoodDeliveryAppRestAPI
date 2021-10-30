89581f557feb8cb1f842b1bcb5ebee47478bbc49
Suvrat <suvrat72000@gmail.com>
2021-10-30 22:35:50 +0530

    Update commit

--- src/main/java/com/food/delivery/RestaurentRepository.java
+++ src/main/java/com/food/delivery/RestaurentRepository.java
@@ -1,5 +1,7 @@
 package com.food.delivery;
 
+
+//Restaurents repository to perform CRUD operations in to the databse.
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RestaurentRepository extends JpaRepository<Restaurent, Integer>{
